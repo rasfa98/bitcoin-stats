5cbd9e5f386c325f1a79c011b102d759661c9a50
Rasmus Falk <rf222fu@student.lnu.se>
2018-11-05 19:04:56 +0100

    Display historical price data as line chart

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
 
 class PriceChart extends Component {
   state = {
     chartData: null
   };
 
-  componentDidMount() {
-    // TODO: Fetch historical data.
+  async componentDidMount() {
+    const URL =
+      'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=EUR&limit=7';
+
+    const res = await axios.get(URL);
+    const data = this.mapHistoricalData(res.data.Data);
+    const chartData = this.getChartData(data.max, data.formatedDates);
+
+    this.setState({ chartData: chartData });
   }
 
+  mapHistoricalData(data) {
+    const max = data.map(x => x.high);
+    const dates = data.map(x => x.time);
+
+    const formatedDates = [];
+
+    dates.forEach(x => {
+      const date = new Date(x * 1000);
+      formatedDates.push(date);
+    });
+
+    return { max: max, formatedDates: formatedDates };
+  }
+
+  getChartData = (data, labels) => {
+    return {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Price change',
+          data: data
+        }
+      ]
+    };
+  };
+
   render() {
-    return <Bar data={this.state.chartData} />;
+    return <Line data={this.state.chartData} />;
   }
 }
 
