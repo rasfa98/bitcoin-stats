a6731274ec3238c7764c3a50d7680dd38aa6ea21
Rasmus Falk <rf222fu@student.lnu.se>
2018-11-05 14:44:14 +0100

    Fetch Bitcoin stats and update state

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Statistics extends Component {
   state = {
     symbol: null,
     price: null,
-    volume: null,
+    total_volume_24_hours: null,
     high_day: null,
     low_day: null,
     last_market: null,
@@ -13,8 +14,35 @@ class Statistics extends Component {
     market_cap: null
   };
 
-  componentDidMount() {
-    // TODO: Fetch bitcoin data.
+  async componentDidMount() {
+    const URL =
+      'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=EUR';
+
+    const res = await axios.get(URL);
+
+    const {
+      FROMSYMBOL,
+      PRICE,
+      TOTALVOLUME24H,
+      HIGHDAY,
+      LOWDAY,
+      LASTMARKET,
+      CHANGEPCT24HOUR,
+      CHANGEPCTDAY,
+      MKTCAP
+    } = res.data.DISPLAY.BTC.EUR;
+
+    this.setState({
+      symbol: FROMSYMBOL,
+      price: PRICE,
+      total_volume_24_hours: TOTALVOLUME24H,
+      high_day: HIGHDAY,
+      low_day: LOWDAY,
+      last_market: LASTMARKET,
+      change_24_hours: CHANGEPCT24HOUR,
+      change_day: CHANGEPCTDAY,
+      market_cap: MKTCAP
+    });
   }
 
   render() {
